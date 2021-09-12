3d5d0f13f2b96f6c738fe7d58016fad04e5d78e8
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-11 17:55:59 -0700

    Update bootloader.asm

--- A:/src/bootloader.asm
+++ A:/src/bootloader.asm
@@ -2,7 +2,3 @@
 ;store this in ROM, not Flash
 
 ssdr $0000, $0000 ;SSD read block 0 to address 0, overwriting this instruction
-
-;only 2 words of ROM, and our bootloader is done!
-;machine code for our loader:
-;0x100000 0x000000
