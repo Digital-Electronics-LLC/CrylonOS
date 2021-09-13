02c5e7f273b3907e856b282a61ce9fa48bd540c9
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-12 18:55:39 -0700

    Update kernel.asm

--- A:/src/Crylix-main/kernel/kernel.asm
+++ A:/src/Crylix-main/kernel/kernel.asm
@@ -91,13 +91,10 @@ store A, 0x400000
 load B, 0x400000
 loadi A, 10
 comp A, B
-je shell_init
+jmpif shell_init, E
 call panic; panic! the memory doesn't work!
 
 shell_init:
   %include A:/bin/shell.asm
-
-jmp the_end; end of OS
-
-the_end:
-  call end
+  call main
+  
