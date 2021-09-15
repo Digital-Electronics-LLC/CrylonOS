5142c83bf20bd2e021ebf0b02358587e0053173c
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-14 17:45:53 -0700

    Update kernel.asm

--- A:/src/Crylix-main/kernel/kernel.asm
+++ A:/src/Crylix-main/kernel/kernel.asm
@@ -25,15 +25,8 @@
 ;|                          |
 ;|--------------------------|
 
-;memory alloctation by DiamondCoder1000
-;location of block allocated stored in X
-;each block is 4096 bytes 
-;256 blocks accessible
 
-malloc:
-  ;unfinished
-  ;still procrastinating
-  ret
+%include A:/src/Crylix-main/kernel/malloc.asm
   
   
 ;|-------------------|
