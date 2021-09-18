b1d0b01f4d077bd6f96c1e52022e5c72a22e478a
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-18 12:03:20 -0700

    Update h.asm

--- A:/bin/h/h.asm
+++ A:/bin/h/h.asm
@@ -1,3 +1,30 @@
 ;the "h" command
 ;i can actually code this, but i have to take an eye break
 ;don't wanna go blind!
+
+h:
+  loadi A, "c"
+  call print_char
+  loadi A, "o"
+  call print_char
+  loadi A, "m"
+  call print_char
+  call print_char
+  loadi A, "a"
+  call print_char
+  loadi A, "n"
+  call print_char
+  loadi A, "d"
+  call print_char
+  loadi A, "s"
+  call print_char
+  loadi A, ":"
+  call print_char
+  loadi A, " "
+  call print_char
+  loadi A, "e"
+  call print_char
+  loadi A, " "
+  call print_char
+  loadi A, "h"
+  call print_char
