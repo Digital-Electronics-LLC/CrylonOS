79c5f7362935954163244106fc54f58ad745007c
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-19 13:18:53 -0700

    Update shell.asm

--- A:/bin/shell.asm
+++ A:/bin/shell.asm
@@ -6,7 +6,7 @@
 %include A:/bin/o/o.asm
 %include A:/src/Crylix-main/kernel/sys_calls/print_char.asm
 %include A:/src/Crylix-main/kernel/sys_calls/input_char.asm
-%include A:/src/Crylix-main/kernel/sys_calls/print_char.asm
+%include A:/src/Crylix-main/kernel/sys_calls/print_string.asm
 
 init:
   loadi A, "s"
