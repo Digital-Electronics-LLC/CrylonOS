3a452b3a1d1856e4d62d3875ab4f9efed6d25d90
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-20 20:39:16 -0700

    Update h.asm

--- A:/bin/h/h.asm
+++ A:/bin/h/h.asm
@@ -1,10 +1,6 @@
 ;the "h" command
-;i can actually code this, but i have to take an eye break
-;don't wanna go blind!
 
-%include A:/src/Crylix-main/kernel/sys_calls/print_char.asm
-
-help_string: data "commands: e h", 0
+help_string      data "commands: e h", 0
 h:
   load help_string, B
   call print_string
