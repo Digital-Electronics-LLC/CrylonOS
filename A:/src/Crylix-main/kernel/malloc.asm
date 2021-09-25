266fe3e50830c0100606d290a6822e6aa128afd3
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-09-25 07:51:02 -0700

    Update malloc.asm

--- A:/src/Crylix-main/kernel/malloc.asm
+++ A:/src/Crylix-main/kernel/malloc.asm
@@ -1,7 +1,7 @@
 ;memory alloctation by DiamondCoder1000
 ;location of block allocated stored in X
 ;each block is 4096 bytes 
-;256 blocks accessible
+;512 blocks accessible
 
 malloc:
   ;unfinished
