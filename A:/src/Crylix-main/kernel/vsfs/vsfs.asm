6dedad220b7bd2c4cd6f42e62c6bd0e2e1183fa9
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-10-03 09:45:11 -0700

    Update vsfs.asm

--- A:/src/Crylix-main/kernel/vsfs/vsfs.asm
+++ A:/src/Crylix-main/kernel/vsfs/vsfs.asm
@@ -2,6 +2,13 @@
 ;very simple file system
 %include A:/src/Crylix-main/kernel/sys_calls/print.asm
 
+fs_init:
+  loadi A, "Mounting filesystem..."
+  call print
+  jump main
+  
+main:
+  
 createFile:
   ;unfinished
   ret
