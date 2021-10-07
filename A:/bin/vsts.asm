5c73dc68719e0c4d8cc8467c49fd3785a61b46fb
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-10-06 20:26:11 -0700

    Update vsts.asm

--- A:/bin/vsts.asm
+++ A:/bin/vsts.asm
@@ -3,7 +3,7 @@
 %include A:/bin/e/exit.asm
 %include A:/bin/d/ls.asm
 %include A:/bin/h/help.asm
-%include A:/bin/o/o.asm
+%include A:/bin/o/open.asm
 %include A:/src/Crylix-main/kernel/sys_calls/print_char.asm
 %include A:/src/Crylix-main/kernel/sys_calls/input_char.asm
 %include A:/src/Crylix-main/kernel/sys_calls/print_string.asm
