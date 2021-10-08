cd8571c2b696977acbd52d6b7aada5b44f26edd8
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-10-07 18:33:37 -0700

    Update help.asm

--- A:/bin/help/help.asm
+++ A:/bin/help/help.asm
@@ -1,7 +1,7 @@
 ;the "h" command
 
-help_string:          data "commands: exit, help", 0
-h:
+help_string:          data "commands: exit, help, ver", 0
+help:
   load help_string, B
   call print_string
   ret
