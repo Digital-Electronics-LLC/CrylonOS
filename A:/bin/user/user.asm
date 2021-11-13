db0eedaf616e2f7d5087ed8f4b32c992ac595c53
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-11-12 20:07:40 -0800

    Update user.asm

--- A:/bin/user/user.asm
+++ A:/bin/user/user.asm
@@ -1,5 +1,6 @@
 ;the "user" command prints the current user using the OS
 ;which is by default and unchangable, "crlnusr" or Crylon User
+%include A:/sys/syscall.asm
 userName:      data "crlnusr"
 user:
   load A, userName
