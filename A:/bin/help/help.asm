deaf86b4e7985d2479a4a21b261eb58db9a08263
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-11-25 10:07:21 -0800

    Update help.asm

--- A:/bin/help/help.asm
+++ A:/bin/help/help.asm
@@ -2,7 +2,7 @@
 
 %include A:/src/Crylix-main/kernel/sys_calls/print.asm
 
-help_string:          data "commands: exit, help, ver", 0
+help_string:          data "commands: exit, help, ver, kilall, user, about", 0
 help:
   load help_string, B
   call print_string
